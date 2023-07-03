e09bacc3d2c3c48a262efd4c2275a1cca2f86ff6
J5402 <137033429+J5402@users.noreply.github.com>
2023-07-03 11:20:37 +0530

    Add files via upload

---
+++ Day  10 &11/do while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = 1;
+
+do {
+  echo " number is: $x <br>";
+  $x++;
+} while ($x <= 7);
+?>
+
+</body>
+</html>
---
+++ Day  10 &11/if else.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = "10";
+$b = "15";
+
+if ($a < $b) {
+  echo "a is less than b!";
+} else {
+  echo "b is less than a!";
+}
+?>
+ 
+</body>
+</html>
