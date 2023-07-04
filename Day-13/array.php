5843aab2fffacc14ef5be82e4b80881dbc7c2cfd
J5402 <137033429+J5402@users.noreply.github.com>
2023-07-04 15:36:44 +0530

    Add files via upload

---
+++ Day-13/array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$fruits = array("mango", "Banana", "cherry"); 
+echo "I like " . $fruits[0] . ", " . $fruits[1] . " and " . $fruits[2] . ".";
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+echo "abc is " . $age['Peter'] . " years old.";
+?>
+
+</body>
+</html>
